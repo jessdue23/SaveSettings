9333627ddb80768a80a36b716f8de5358170aec1
Jessica Smith <jduesing23@gmail.com>
2024-10-12 07:42:02 -0700

    Save username preferences

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace SaveStuff;
+
+public partial class MainPage : ContentPage
+{
+    
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+
+    private void Save_OnClicked(object sender, EventArgs e)
+    {
+        Preferences.Set("Username", txtUsername.Text);
+    }
+
+    private void Read_OnClicked(object sender, EventArgs e)
+    {
+
+        txtUsername.Text = Preferences.Get("Username", "");
+    }
+
+    private void Clear_OnClicked(object sender, EventArgs e)
+    {
+        txtUsername.Text = string.Empty;
+    }
+}
